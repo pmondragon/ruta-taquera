644ac84254a11f4c48ba0d26502ff0ab9abd5a84
Pedro <petermoca2@gmail.com>
2018-05-05 04:27:33 -0500

    Adding requirements.txt and menu/templates

--- src/menu/views.py
+++ src/menu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
-from django.http import HttpResponse
 
-def homePageView(request):
-    return HttpResponse('Hello World')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
